af5bf82e9ff6590959bc5898679f86905053373e
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2022-10-31 05:19:43 -0600

    Start
    Start

--- hangman-assignment/js/index.js
+++ hangman-assignment/js/index.js
@@ -1,9 +1,32 @@
-/**
- För att toggla SVG:en
- document.querySelector('figure').classList.add('scaffold')
- document.querySelector('figure').classList.add('head')
- document.querySelector('figure').classList.add('body')
- document.querySelector('figure').classList.add('arms')
- document.querySelector('figure').classList.add('legs')
+//  För att toggla SVG:en
+document.querySelector("figure").classList.add("scaffold");
+document.querySelector("figure").classList.add("head");
+document.querySelector("figure").classList.add("body");
+document.querySelector("figure").classList.add("arms");
+document.querySelector("figure").classList.add("legs");
 
- */
+let words = ["dog", "cat", "duck", "volvo", "horse", "apple", "lizard"];
+let rndWord = "";
+
+function guessWord() {
+  let rndWord = Math.floor(Math.random() * words.length);
+  return;
+}
+
+const button = document.getElementById("btn");
+button.addEventListener("click", () => {
+  let userChar = document.getElementById("charInput").value;
+  document.getElementById("charInput").value = "";
+
+  console.log(userChar);
+});
+
+console.log(guessWord());
+
+// let userInput = prompt("Gissa på ett ord");
+
+//if (prompt === words) {
+//prompt("Du lyckades!");
+//} else {
+//prompt("fortsätt gissa");
+//}
