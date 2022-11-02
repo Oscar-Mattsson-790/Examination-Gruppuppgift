47407ddb6c8ec788b1c054ea516bfb6494692b2f
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2022-11-02 06:07:20 -0600

    Made stuff

--- hangman-assignment/js/index.js
+++ hangman-assignment/js/index.js
@@ -1,18 +1,15 @@
-//  För att toggla SVG:en
-document.querySelector("figure").classList.add("scaffold");
-document.querySelector("figure").classList.add("head");
-document.querySelector("figure").classList.add("body");
-document.querySelector("figure").classList.add("arms");
-document.querySelector("figure").classList.add("legs");
+const hangman = ["scaffold", "head", "body", "arms", "legs"];
+const button = document.querySelector("#btn");
+const totalGuesses = 5;
 
 let words = ["dog", "cat", "duck", "volvo", "horse", "apple", "lizard"];
 let rndWord = "";
 let guessedChars = "";
-let showWord = document.querySelector("#hold");
 let pickWord = "";
 let lastInput = "";
-const button = document.querySelector("#btn");
+let wrongAnswerCount = 0;
 
+// Tar fram slumpat ord från arrayen "words"
 function getWord() {
   let rndWord = Math.floor(Math.random() * words.length);
   pickWord = words[rndWord];
@@ -21,6 +18,7 @@ function getWord() {
   printUnderscores(pickWord);
 }
 
+// Tar fram antal undersocre per bokstav för ordet
 function printUnderscores(pickWord) {
   let underscoreVar = "";
   for (i = 0; i < pickWord.length; i++) {
@@ -30,7 +28,9 @@ function printUnderscores(pickWord) {
   document.querySelector(".secretWord").innerText = underscoreVar;
 }
 
+// Inmatning av bokstäver
 function controlChars(input) {
+  input = input.toUpperCase();
   lastInput = input;
   if (guessedChars.length === 0) {
     guessedChars = input;
@@ -54,9 +54,11 @@ function controlChars(input) {
   }
 }
 
+// Kontrollerar om användaren skrivit in en bokstav som stämmer överens med någon bokstav i det förutbestämda/slumpade ordet. Om så är fallet byts motsvarande underscore ut mot den rätta bokstaven
 function switchLetter() {
   let switchVar = document.querySelector(".secretWord").innerText;
   let tmpArray = [];
+  let correctAnswerCount = 0;
 
   // Skapar array med underscores
   for (i = 0; i < pickWord.length; i++) {
@@ -65,9 +67,18 @@ function switchLetter() {
 
   // Byter ut bokstäverna i arrayen om användaren gissar rätt
   for (i = 0; i < pickWord.length; i++) {
-    if (lastInput === pickWord[i]) {
-      tmpArray[i] = pickWord[i];
+    if (lastInput === pickWord[i].toUpperCase()) {
+      tmpArray[i] = pickWord[i].toUpperCase();
+      correctAnswerCount++;
     }
+  }
+
+  /* Om bokstaven användaren skrev in inte stämmer överens med någon av bokstäverna i det slumpade ordet så ritas nästa
+     del av "hänggubben" ut och en mätare plussas på för att hålla koll på hur många felgissningar som gjorts */
+  if (correctAnswerCount == 0) {
+    document.querySelector("figure").classList.add(hangman[wrongAnswerCount]);
+    wrongAnswerCount++;
+    totalWrongAnswers(wrongAnswerCount);
   }
 
   // Skapar en ny variabel med bokstäver som bytts ut
@@ -81,39 +92,18 @@ function switchLetter() {
   document.querySelector(".secretWord").innerText = switchVar;
 }
 
-var alphabet = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
-
+// Om/När användaren klickar på knappen sparas inmatningen i en variabel och skickas vidare för kontroll, samt nollställer värdet i inmatningsfältet
 button.addEventListener("click", () => {
   let userChar = document.getElementById("charInput").value;
   controlChars(userChar);
   document.getElementById("charInput").value = "";
 });
+
+function totalWrongAnswers(wrongAnswerCount) {
+  if (wrongAnswerCount === totalGuesses) {
+    button.textContent = "Play Again?";
+    console.log("YOU LOSE");
+  }
+}
 
 getWord();
